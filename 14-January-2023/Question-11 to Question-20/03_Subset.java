dd0d84b5ca8f26604d51b160dfcca5e628bf5393
Dipendra Singh <91822875+Dipendra6377@users.noreply.github.com>
2023-01-14 17:12:37 +0530

    Create 03_Subset.java

---
+++ 14-January-2023/Question-11 to Question-20/03_Subset.java
@@ -0,0 +1,24 @@
+class Solution {
+    List<List<Integer>> result;
+    public List<List<Integer>> subsets(int[] nums) {
+       result = new ArrayList<>();
+       Arrays.sort(nums);
+        subset(nums,new ArrayList<>(),0);
+        return result;
+    }
+
+    public void subset(int[] nums,ArrayList<Integer> list,int i){
+        if(i==nums.length){
+            result.add(new ArrayList<>(list));
+            return;
+        }
+
+        list.add(nums[i]);
+        subset(nums,list,i+1);
+        list.remove(list.size()-1);
+        subset(nums,list,i+1);
+    }
+}
+
+
+// can also be done by bit manipulation will be doing after bit manipulation
