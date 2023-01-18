f40fc72c58fa085728c0e47a366fcbcb85752d1c
Dipendra Singh <91822875+Dipendra6377@users.noreply.github.com>
2023-01-18 18:01:02 +0530

    Update 06_word Break.java

--- 16 January 2023/Question 21 to Question 30/06_word Break.java
+++ 16 January 2023/Question 21 to Question 30/06_word Break.java
@@ -16,3 +16,23 @@ class Solution {
         return map.get(s);
     }
 }
+
+//Iterative
+
+class Solution {
+    
+    public boolean wordBreak(String s, List<String> word) {
+        Set<String> set=new HashSet<>(word);
+        boolean dp[]=new boolean[s.length()+1];
+        dp[0]=true;
+        for(int i=1;i<=s.length();i++){
+            for(int j=0;j<i;j++){
+                if(dp[j]&&set.contains(s.substring(j,i))){
+                    dp[i]=true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
